5807b3505eea847d56a3f32879cb373664958fd8
Alexander Hidvéghy <alexanderhidveghy@Mac.home>
2025-12-10 14:53:50 +0100

    Rename image files and update references in HTML, update footer link

--- js/components/footer.js
+++ js/components/footer.js
@@ -140,7 +140,7 @@ function loadFooter() {
                         <a href="/o-nas">O nás</a>
                         <a href="/produkty/dvere/seria-a">Produkty</a>
                         <a href="/kontakt">Kontakt</a>
-                        <a href="/referencie">Portfólio</a>
+                        <a href="/referencie">Referencie</a>
                     </div>
                    
                     <div class="footer-contact">
